5961d67b91a05b566f1aa7a248b5a7ab0b1e38c2
AniKal25 <kal21006@byui.edu>
2022-09-30 21:49:32 +0100

    Push

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
+const express = require('express');
 const routes = require('express').Router();
 const event = require('../routes/event');
 const location = require('../routes/location');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 
 routes.use('/event', event);
 routes.use('/location', location);
 
-
+routes.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 routes.get('/', (req, res) => {
     res.send('This is working as it should! => We will move forward to the project now 💀');
